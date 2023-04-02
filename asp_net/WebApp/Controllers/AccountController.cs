27b4d062fd8a8d3d15e76f98cc2bfcc04d27027a
p <p@DESKTOP-UBUDTBS>
2023-04-02 10:44:39 +0200

    refactored away from hans solution

---
+++ asp_net/WebApp/Controllers/AccountController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace WebApp.Controllers
+{
+    public class AccountController : Controller
+    {
+        public IActionResult Login()
+        {
+            ViewData["Title"] = "Login";
+            return View();
+        }
+        public IActionResult SignUp()
+        {
+            ViewData["Title"] = "Sign up";
+            return View();
+        }
+    }
+}
